e238840e4adc2636a57624bee21c75d5e897a3f2
drupchen <hhdrupchen@gmail.com>
2022-01-23 16:16:34 +0100

    update

--- leavedonto/ontomanager.py
+++ leavedonto/ontomanager.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import yaml
+
 from .leavedonto import LeavedOnto
 from .tag_to_onto import generate_to_tag, tagged_to_trie, get_entries
 
@@ -141,3 +143,51 @@ class OntoManager:
                 else:
                     to_organize.append(r)
         return to_update, to_organize
+
+    def adjust_legends(self):
+        template = "# legend list from the original onto.\n" \
+                   "legend_orig: {orig}\n" \
+                   "# new legend:\n" \
+                   "# omitting elements will remove the whole field, together with all its content.\n" \
+                   "# adding elements will add empty fields for all entries.\n" \
+                   "# reordering elements will reorder all the entries.\n" \
+                   "legend_new: {orig}\n" \
+                   "# list of tuple(<old>, <new>): <old> becomes <new>, keeping all the content in the fields.\n" \
+                   "# note: leave empty if there are no replacements.\n" \
+                   "replacements: []"
+        to_adjust = yaml.safe_dump(template.format(orig=self.onto1.ont.legend))
+
+        legend_config = Path("adjust_legends.yaml")
+        if not legend_config.is_file():
+            legend_config.write_text(to_adjust)
+            print(f"Please fill in the adjustment file: {legend_config}")
+            return
+
+        adjust = yaml.safe_load(legend_config.read_text())
+        if to_adjust == adjust:
+            print("Please modify adjust_legends.yaml and rerun.")
+
+        self._adjust_entries(adjust["legend_orig"], adjust["legend_new"])
+        self._replace_legend(adjust["legend_new"], adjust["replacements"])
+
+    def _replace_legend(self, l_new, replc):
+        # apply replacements
+        for orig, new in replc:
+            for n, el in enumerate(l_new):
+                if orig == el:
+                    l_new[n] = new
+        self.onto1.ont.legend = l_new
+
+    def _adjust_entries(self, l_orig, l_new):
+        queue = [self.onto1.ont.head]
+        while queue:
+            current_node = queue.pop()
+            if current_node.leaf:
+                # remove duplicates and sort in tibetan order
+                for n, entry in enumerate(current_node.data):
+                    old = {l_orig[i]: entry[i] for i in range(len(l_orig))}
+                    new = {l_new[i]: "" for i in range(len(l_new))}  # no values
+                    new = {l: old[l] if l in old else "" for l, _ in new.items()}  # with values
+                    new_entry = [new[e] for e in l_new]
+                    current_node.data[n] = new_entry
+            queue = [node for key, node in current_node.children.items()] + queue
